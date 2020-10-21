78f056a8f61c1f64fb47a20de1476511f35f2e35
pookee777 <71640721+pookee777@users.noreply.github.com>
2020-10-21 11:57:40 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,37 @@
+class SlingShot
+{
+    constructor(bodyA,pointB)
+    {
+        var options = 
+        {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 3
+        }        
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly()
+    {
+        this.sling.bodyA = null;
+    }
+
+    display()
+    {        
+        if(this.sling.bodyA)
+        {
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            
+            push();
+            strokeWeight(4);
+            stroke("white");
+            fill("white");
+            line(pointA.x-50, pointA.y-80, pointB.x-50, pointB.y-80);
+            pop();
+        }
+    }
+}
